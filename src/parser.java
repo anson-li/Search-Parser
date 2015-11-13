e7fbce0f8ebb8119220406b08fd685e09573cfd4
Anson Li <ansonli.94@gmail.com>
2015-11-13 10:34:41 -0700

    sample text parser

--- src/parser.java
+++ src/parser.java
@@ -27,12 +27,46 @@
 **/
 
 import com.sleepycat.db.*
+import java.io.*;
 
 public class parser {
 
 public static void main(String[] args){
 
 	try {
+
+		// sample code used to read file
+		// taken from http://www.java-tips.org/java-se-tips-100019/18-java-io/2028-how-to-read-file-in-java.html
+		File file = new File("data.txt");
+		FileInputStream fis = null;
+    	BufferedInputStream bis = null;
+   		DataInputStream dis = null;
+
+   		try {
+	      fis = new FileInputStream(file);
+	 
+	      // Here BufferedInputStream is added for fast reading.
+	      bis = new BufferedInputStream(fis);
+	      dis = new DataInputStream(bis);
+	 
+	      // dis.available() returns 0 if the file does not have more lines.
+	      while (dis.available() != 0) {
+	 
+	      // this statement reads the line from the file and print it to
+	        // the console.
+	        System.out.println(dis.readLine());
+	      }
+	 
+	      // dispose all the resources after using them.
+	      fis.close();
+	      bis.close();
+	      dis.close();
+	 
+	    } catch (FileNotFoundException e) {
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
 
 		// demo code
 		//database configuration
