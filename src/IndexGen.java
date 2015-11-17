d703bc48764a7caa8c08853b2b48ca03d601af2e
Anson Li <ansonli.94@gmail.com>
2015-11-17 09:56:25 -0700

    debugger statement for exec

--- src/IndexGen.java
+++ src/IndexGen.java
@@ -11,23 +11,23 @@ public class IndexGen{
 	public static void main(String[] args){
 		// Sorts text file and puts it on stdin / perl sorts / db_load parses into hash
 		IndexGen shell = new IndexGen();
-		shell.executeCommand("sort -u reviews.txt | perl break.pl | db_load -c duplicates=1 rw.idx -T -t hash");
-		System.out.println("RW.IDX : Process complete!");
+		String output;
+		output = shell.executeCommand("sort -u reviews.txt | perl break.pl | db_load -c duplicates=1 rw.idx -T -t hash");
+		System.out.println("RW.IDX : Process complete! Result: " + output);
 
-		shell.executeCommand("sort -u pterms.txt | perl break.pl | db_load -c duplicates=1 pt.idx -T -t btree");
-		System.out.println("PT.IDX : Process complete!");
+		output = shell.executeCommand("sort -u pterms.txt | perl break.pl | db_load -c duplicates=1 pt.idx -T -t btree");
+		System.out.println("PT.IDX : Process complete! Result: " + output);
 
-		shell.executeCommand("sort -u rterms.txt | perl break.pl | db_load -c duplicates=1 rt.idx -T -t btree");
-		System.out.println("RT.IDX : Process complete!");
+		output = shell.executeCommand("sort -u rterms.txt | perl break.pl | db_load -c duplicates=1 rt.idx -T -t btree");
+		System.out.println("RT.IDX : Process complete! Result: " + output);
 
-		shell.executeCommand("sort -u scores.txt | perl break.pl | db_load -c duplicates=1 sc.idx -T -t btree");	
-		System.out.println("SC.IDX : Process complete!");
+		output = shell.executeCommand("sort -u scores.txt | perl break.pl | db_load -c duplicates=1 sc.idx -T -t btree");	
+		System.out.println("SC.IDX : Process complete! Result: " + output);
 	}
 
 	//http://www.mkyong.com/java/how-to-execute-shell-command-from-java/
 	private String executeCommand(String command) {
 		StringBuffer output = new StringBuffer();
-
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
