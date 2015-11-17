b0029171d04355bec4dabdf61e3f9f0f725690cd
Anson Li <ansonli.94@gmail.com>
2015-11-17 10:15:09 -0700

    debugger worked ; part 2 cleanup

--- src/IndexGen.java
+++ src/IndexGen.java
@@ -12,20 +12,10 @@ public class IndexGen{
 		// Sorts text file and puts it on stdin / perl sorts / db_load parses into hash
 		IndexGen shell = new IndexGen();
 		String output;
-		output = shell.executeCommand("ls");
-		System.out.println("Output: " + output);
-
 		output = shell.executeCommand("sort -u reviews.txt | perl break.pl | db_load -c duplicates=1 rw.idx -T -t hash");
-		System.out.println("RW.IDX : Process complete! Result: " + output);
-
 		output = shell.executeCommand("sort -u pterms.txt | perl break.pl | db_load -c duplicates=1 pt.idx -T -t btree");
-		System.out.println("PT.IDX : Process complete! Result: " + output);
-
 		output = shell.executeCommand("sort -u rterms.txt | perl break.pl | db_load -c duplicates=1 rt.idx -T -t btree");
-		System.out.println("RT.IDX : Process complete! Result: " + output);
-
 		output = shell.executeCommand("sort -u scores.txt | perl break.pl | db_load -c duplicates=1 sc.idx -T -t btree");	
-		System.out.println("SC.IDX : Process complete! Result: " + output);
 	}
 
 	//http://www.mkyong.com/java/how-to-execute-shell-command-from-java/
