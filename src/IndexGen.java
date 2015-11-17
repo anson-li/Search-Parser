20b2abf469a6ce903f091c1fb476edec1c508eda
Anson Li <ansonli.94@gmail.com>
2015-11-17 09:05:45 -0700

    sample bash script gen

--- src/IndexGen.java
+++ src/IndexGen.java
@@ -12,6 +12,17 @@ public class IndexGen{
 		shell.executeCommand("sort -u -o pterms.txt pterms.txt");
 		shell.executeCommand("sort -u -o scores.txt scores.txt");
 
+		/* shell commands to be used when generating rdx:
+		** Sorts text file and puts it on stdin / perl sorts / db_load parses into hash
+		** First one is tested ; others are demo
+		sort -u reviews.txt | perl break.pl | db_load -c duplicates=1 rw.idx -T -t hash
+		sort -u pterms.txt | perl break.pl | db_load -c duplicates=1 pt.idx -T -t btree
+		sort -u rterms.txt | perl break.pl | db_load -c duplicates=1 rt.idx -T -t btree
+		sort -u scores.txt | perl break.pl | db_load -c duplicates=1 sc.idx -T -t btree
+
+		** Test .idx files:
+		db_dump rw.idx -da
+		*/
 	}
 
 	//http://www.mkyong.com/java/how-to-execute-shell-command-from-java/
