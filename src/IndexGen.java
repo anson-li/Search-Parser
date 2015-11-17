b7a1f9d2f9c457908da9126b1d3584ebc2668b32
Anson Li <ansonli.94@gmail.com>
2015-11-16 23:17:30 -0700

    execute shell command init

--- src/IndexGen.java
+++ src/IndexGen.java
@@ -6,46 +6,11 @@ import java.lang.*;
 public class IndexGen{
 
 public static void main(String[] args){
+	// have to execute everything through the following process:
+	ExecuteShellCommand shell = new ExecuteShellCommand();
+	shell.executeCommand("sort -u -o rterms.txt rterms.txt");
+	shell.executeCommand("sort -u -o pterms.txt pterms.txt");
+	shell.executeCommand("sort -u -o scores.txt scores.txt");
 
-	BufferedReader br = null;
 
-	try {
-		//database configuration
-		DatabaseConfig dbConfig = new DatabaseConfig();
-		dbConfig.setType(DatabaseType.HASH);
-		dbConfig.setAllowCreate(true);
-		dbConfig.setSortedDuplicates(true); // setting flag for allowing duplicates
-		
-		//Create a database 
-		Database std_db = new Database("rw.idx", null, dbConfig);
-		OperationStatus oprStatus;
-
-		//Inserting Data into a database
-		DatabaseEntry key = new DatabaseEntry();
-		DatabaseEntry data = new DatabaseEntry();
-
-		br = new BufferedReader(new FileReader("./data.txt"));
-		String sCurrentLine;
-		while ((sCurrentLine = br.readLine()) != null) {
-
-			String id = sCurrentLine.split(",", 2)[0];
-			String value = sCurrentLine.split(",", 2)[1];
-
-			data.setData(value.getBytes());
-			data.setSize(value.length()); 
-			key.setData(id.getBytes()); 
-			key.setSize(id.length());
-			oprStatus = std_db.put(null, key, data);
-
-		}
-
-		// Closing the connection
-    	std_db.close();
-
-  	} // end of try 
-
-  	catch (Exception ex) 
-   		{ ex.getMessage();} 
-
-	}
 }
