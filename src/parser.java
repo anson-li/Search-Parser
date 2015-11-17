0f75d3707becbff9e2fee467770a5243fca0cf01
Satyen Akolkar <s.akolkar3@gmail.com>
2015-11-17 13:13:37 -0700

    added replace for second half of split

--- src/parser.java
+++ src/parser.java
@@ -85,7 +85,7 @@ public class parser {
 				// currentline holds the full value, but can be a simple space
 				if (!sCurrentLine.equals("")) {
 					String replaced = sCurrentLine.split(": ", 2)[0].replaceAll("\"", "&quot;").replaceAll("\\\\","\\\\\\\\");
-					String value = sCurrentLine.split(": ", 2)[1]; // doesn't account for any further splits...
+					String value = sCurrentLine.split(": ", 2)[1].replaceAll("\"", "&quot;").replaceAll("\\\\","\\\\\\\\"); // doesn't account for any further splits...
 					switch (replaced) {
 						case "product/productId":
 							product.setID(value);
