b49a1bd3b4d95a2062ef78c2e8b7c29445121412
Satyen Akolkar <s.akolkar3@gmail.com>
2015-11-18 15:01:26 -0700

    phase 1: working for all test sets

---
+++ src/compare.sh
@@ -0,0 +1,8 @@
+echo "Comparing reviews.txt..."
+diff reviews.txt ~drafiei/291/pub/100k/reviews.txt
+echo "Comparing pterms.txt..."
+diff pterms.txt ~drafiei/291/pub/100k/pterms.txt
+echo "Comparing scores.txt..."
+diff scores.txt ~drafiei/291/pub/100k/scores.txt
+echo "Comparing rterms.txt..."
+diff rterms.txt ~drafiei/291/pub/100k/rterms.txt
