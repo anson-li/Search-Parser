d629afdbe6af10f191bcd6d37bf9a82c1cc04d2f
Satyen Akolkar <s.akolkar3@gmail.com>
2015-11-18 21:29:51 -0700

    starting refactor

---
+++ src/DataFileGenerator.java
@@ -0,0 +1,37 @@
+/**
+ * TODO: fill me :)
+ * @author akolkar
+ *
+ */
+public class DataFileGenerator {
+	
+	/**
+	 * TODO: fill me :)
+	 * @author akolkar
+	 *
+	 */
+	private class Product {
+		private String productId;
+	    private String title;
+	    private String price;
+
+	    public void setID(String data) {
+            productId = data;
+	    }
+
+	    public void setTitle(String data) {
+            title = data;
+	    }
+
+	    public void setPrice(String data) {
+            price = data;
+	    }
+
+	    public String getID() { return productId; }
+	    public String getTitle() { return title; }
+	    public String getPrice() { return price; }
+	}
+	
+	
+
+}
