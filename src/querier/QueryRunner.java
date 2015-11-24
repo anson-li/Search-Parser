e3894b2648a7556bbcd17e3e7ae95eb91fc1bb7f
Anson Li <ansonli.94@gmail.com>
2015-11-24 13:21:09 -0700

    Revert "basic javadoc comment"
    This reverts commit c394df76d3bd0274be237b785a505356774d84a4.

--- src/querier/QueryRunner.java
+++ src/querier/QueryRunner.java
@@ -8,10 +8,6 @@ import exceptions.DBMSExitException;
 
 public class QueryRunner {
 
-    /**
-    * QueryRunner forms the UI section of the query
-    * Parses the users' request and sends to DBMS to process.
-    */
 	public static void main(String[] args) {
 	    
         DBMS dbHandler = null;    
@@ -38,7 +34,6 @@ public class QueryRunner {
             	return;
             }
             
-            // resets for additional queries
             dbHandler.processQuery(userQuery);
             dbHandler.reset();
         }
