5f7359b92c5accffee14d89a8c2fa0872a625398
Satyen <s.akolkar3@gmail.com>
2015-11-24 12:10:12 -0700

    fixed wildcard search

--- src/querier/DBMS.java
+++ src/querier/DBMS.java
@@ -368,7 +368,7 @@ public class DBMS {
                 } else {
                 	check_file += "'rterms.txt' 'pterms.txt'";
                 }
-            	for ( String match : shell.executeCommand("grep -h \""+ subquery.toLowerCase().replace("%", "") +"[[:alpha:]]*\" "+ check_file+" | sort -t, -k 2,2n -u | grep -o \""+ subquery.toLowerCase().replace("%", "") +"[[:alpha:]]*\"").split("\n")) {
+            	for ( String match : shell.executeCommand("grep -h \""+ subquery.toLowerCase().replace("%", "") +"[[:alpha:]]*\" "+ check_file+" | sort -t, -k 2,2n -u | grep -o \""+ subquery.toLowerCase().replace("%", "") +"[[:alpha:]]*\" | uniq").split("\n")) {
 					queryPTerms(match, next_result_indices);
 					queryRTerms(match, next_result_indices);
 				}
